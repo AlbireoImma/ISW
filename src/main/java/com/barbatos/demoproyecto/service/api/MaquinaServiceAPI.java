ac90a6fb3724f8c4352cb381de7f1cd777e0ec9d
AlbireoImma <faam1612@gmail.com>
2019-07-29 14:06:24 -0400

    Revert "Add files via upload"

--- src/main/java/com/barbatos/demoproyecto/service/api/MaquinaServiceAPI.java
+++
@@ -1,8 +0,0 @@
-package com.barbatos.demoproyecto.service.api;
-
-import com.barbatos.demoproyecto.commons.GenericServiceAPI;
-import com.barbatos.demoproyecto.model.Maquina;
-
-public interface MaquinaServiceAPI extends GenericServiceAPI<Maquina, Long>{
-
-}
